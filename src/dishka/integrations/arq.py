b1dc631678b84048a2651896766976743dbe504a
shalmeo <algaevshamil8@gmail.com>
2024-02-24 14:35:22 +0300

    arq dishka setup no longer override existing worker hooks

--- src/dishka/integrations/arq.py
+++ src/dishka/integrations/arq.py
@@ -7,6 +7,7 @@ import logging
 from typing import Any, Final
 
 from arq import Worker
+from arq.typing import StartupShutdown
 
 from dishka.async_container import AsyncContainer
 from dishka.integrations.base import wrap_injection
@@ -25,21 +26,30 @@ def inject(func):
     )
 
 
-async def job_start(context: dict[Any, Any]):
-    container: AsyncContainer = context["__container__"]
-    sub_container = await container().__aenter__()
-    context[DISHKA_CONTAINER_KEY] = sub_container
+def job_start(hook_func: StartupShutdown | None):
+    async def wrapper(context: dict[Any, Any]) -> None:
+        container: AsyncContainer = context["__container__"]
+        sub_container = await container().__aenter__()
+        context[DISHKA_CONTAINER_KEY] = sub_container
+
+        if hook_func:
+            await hook_func(context)
+
+    return wrapper
 
 
-async def job_end(context: dict[Any, Any]) -> None:
-    sub_container: AsyncContainer = context[DISHKA_CONTAINER_KEY]
-    await sub_container.close()
+def job_end(hook_func: StartupShutdown | None):
+    async def wrapper(context: dict[Any, Any]) -> None:
+        if hook_func:
+            await hook_func(context)
+
+        sub_container: AsyncContainer = context[DISHKA_CONTAINER_KEY]
+        await sub_container.close()
+
+    return wrapper
 
 
 def setup_dishka(container: AsyncContainer, worker: Worker) -> None:
-    if any((worker.on_job_start, worker.on_job_end)):
-        logger.warning("Dishka setup will override existing worker hooks")
-
     worker.ctx["__container__"] = container
-    worker.on_job_start = job_start
-    worker.on_job_end = job_end
+    worker.on_job_start = job_start(worker.on_job_start)
+    worker.on_job_end = job_end(worker.on_job_end)

3a21c5e4b52cb6620f5d176ea5d3e5e344b70d30
Nikita Pastukhov <diementros@yandex.ru>
2024-02-25 10:53:16 +0300

    feat (#71): add FastStream integration

---
+++ src/dishka/integrations/faststream.py
@@ -0,0 +1,64 @@
+from collections.abc import Callable
+from contextlib import asynccontextmanager
+from typing import Any, AsyncIterator, ParamSpec, TypeVar
+
+from faststream import BaseMiddleware, FastStream, context
+from faststream.types import DecodedMessage
+
+from dishka.async_container import AsyncContainer
+from dishka.integrations.base import Depends, wrap_injection
+
+__all__ = (
+    "Depends",
+    "inject",
+    "setup_dishka",
+)
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def inject(func: Callable[P, T]) -> Callable[P, T]:
+    return wrap_injection(
+        func=func,
+        container_getter=lambda *_: context.get_local("dishka"),
+        is_async=True,
+        remove_depends=True,
+    )
+
+
+class DishkaMiddleware(BaseMiddleware):
+    def __init__(self, container: AsyncContainer) -> None:
+        self.container = container
+
+    def __call__(self, msg: Any | None = None) -> "DishkaMiddleware":
+        self.msg = msg
+        return self
+
+    @asynccontextmanager
+    async def consume_scope(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> AsyncIterator[DecodedMessage]:
+        async with self.container() as request_container:
+            with context.scope("dishka", request_container):
+                async with super().consume_scope(*args, **kwargs) as result:
+                    yield result
+
+
+def setup_dishka(
+    container: AsyncContainer,
+    app: FastStream,
+    *,
+    finalize_container: bool = True,
+) -> None:
+    assert app.broker, "You can't patch FastStream application without broker"  # noqa: S101
+
+    if finalize_container:
+        app.after_shutdown(container.close)
+
+    app.broker.middlewares = (
+        *app.broker.middlewares,
+        DishkaMiddleware(container),
+    )
---
+++ tests/integrations/faststream/test_faststream.py
@@ -0,0 +1,74 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+from typing import Annotated
+from unittest.mock import Mock
+
+import pytest
+from faststream import FastStream
+from faststream.nats import NatsBroker, TestNatsBroker
+
+from dishka import make_async_container
+from dishka.integrations.faststream import (
+    Depends,
+    inject,
+    setup_dishka,
+)
+from ..common import (
+    APP_DEP_VALUE,
+    REQUEST_DEP_VALUE,
+    AppDep,
+    AppProvider,
+    RequestDep,
+)
+
+
+@asynccontextmanager
+async def dishka_app(view, provider) -> AsyncIterator[NatsBroker]:
+    broker = NatsBroker()
+    broker.subscriber("test")(inject(view))
+
+    app = FastStream(broker)
+
+    container = make_async_container(provider)
+    setup_dishka(container, app=app)
+
+    async with TestNatsBroker(broker) as br:
+        yield br
+
+    await container.close()
+
+
+async def get_with_app(
+    a: Annotated[AppDep, Depends()],
+    mock: Annotated[Mock, Depends()],
+) -> str:
+    mock(a)
+    return "passed"
+
+
+@pytest.mark.asyncio
+async def test_app_dependency(app_provider: AppProvider):
+    async with dishka_app(get_with_app, app_provider) as client:
+        assert await client.publish("", "test", rpc=True) == "passed"
+
+        app_provider.mock.assert_called_with(APP_DEP_VALUE)
+        app_provider.app_released.assert_not_called()
+    app_provider.app_released.assert_called()
+
+
+async def get_with_request(
+    a: Annotated[RequestDep, Depends()],
+    mock: Annotated[Mock, Depends()],
+) -> str:
+    mock(a)
+    return "passed"
+
+
+
+@pytest.mark.asyncio
+async def test_request_dependency(app_provider: AppProvider):
+    async with dishka_app(get_with_request, app_provider) as client:
+        assert await client.publish("", "test", rpc=True) == "passed"
+
+        app_provider.mock.assert_called_with(REQUEST_DEP_VALUE)
+        app_provider.request_released.assert_called_once()

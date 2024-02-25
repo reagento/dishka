2e55d44185446067038925c6df2259e408fd5339
Andrey Tikhonov <17@itishka.org>
2024-02-26 00:37:57 +0100

    Fix async container

--- src/dishka/async_container.py
+++ src/dishka/async_container.py
@@ -136,7 +136,7 @@ class AsyncContainer:
         if not factory:
             if not self.parent_container:
                 raise NoFactoryError(key)
-            return self.parent_container.get(key.type_hint, key.component)
+            return await self.parent_container.get(key.type_hint, key.component)
         return await self._get_from_self(factory, key)
 
     async def close(self):

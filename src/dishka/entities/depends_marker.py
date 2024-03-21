3d1a9d49c2a8d30e62a03e9ecab96a3c7a811b88
Andrey Tikhonov <17@itishka.org>
2024-03-21 22:21:55 +0100

    FromDishka as generic

--- src/dishka/entities/depends_marker.py
+++ src/dishka/entities/depends_marker.py
@@ -1,6 +1,32 @@
+import warnings
+from typing import Annotated, TYPE_CHECKING
+
 from .component import DEFAULT_COMPONENT, Component
+from .key import FromComponent
 
+if TYPE_CHECKING:
+    from typing import Union as FromDishka
+else:
+    class FromDishka:
+        def __init__(self, component: Component = None):
+            if component is None:
+                self.component = DEFAULT_COMPONENT
+                warnings.warn(
+                    "Annotated[Cls, FromDishka()] is deprecated "
+                    "use `FromDishka[Cls]` or "
+                    "`Annotated[Cls, FromComponent()]` instead",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
 
-class FromDishka:
-    def __init__(self, component: Component = DEFAULT_COMPONENT):
-        self.component = component
+            else:
+                self.component = component
+                warnings.warn(
+                    "Annotated[Cls, FromDishka(component)] is deprecated "
+                    "use `Annotated[Cls, FromComponent(component)]` instead",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+
+        def __class_getitem__(cls, item):
+            return Annotated[item, FromComponent()]

6bb3ba7d114cf177db018561c13bf37c4fa01f7a
lubaskincode <lubaskincorporation@gmail.com>
2024-03-28 19:39:36 +0300

    Implement pass exception to generators #113

--- tests/unit/container/test_enter_exit.py
+++ tests/unit/container/test_enter_exit.py
@@ -1,3 +1,6 @@
+from collections.abc import AsyncGenerator, Generator
+from unittest.mock import Mock
+
 import pytest
 
 from dishka import (
@@ -9,6 +12,7 @@ from dishka import (
 )
 from ..sample_providers import (
     ClassA,
+    async_gen_a,
     sync_gen_a,
 )
 
@@ -99,6 +103,45 @@ def test_sync_enter(provide_scope, start_scope, expected_scope):
         (Scope.REQUEST, None, Scope.REQUEST),
     ],
 )
+def test_error_in_sync_enter(provide_scope, start_scope, expected_scope):
+    exception = ValueError("Some exception")
+    mock = Mock()
+
+    class MyProvider(Provider):
+        a = provide(sync_gen_a, scope=provide_scope)
+
+        @provide(scope=provide_scope)
+        def get_int(self) -> Generator[None, int, None]:
+            caught_exc = yield 100
+            mock(caught_exc)
+
+    base_container = make_container(MyProvider())
+
+    try:
+        with base_container(scope=start_scope) as container:
+            assert container.registry.scope is expected_scope
+            a = container.get(ClassA)
+            assert a
+            assert a.dep == 100
+
+            raise exception
+    except ValueError as exc:
+        if exc != exception:
+            raise
+
+    mock.assert_called_with(exception)
+    assert a.closed
+
+
+@pytest.mark.parametrize(
+    ("provide_scope", "start_scope", "expected_scope"),
+    [
+        (Scope.SESSION, Scope.REQUEST, Scope.REQUEST),
+        (Scope.REQUEST, Scope.REQUEST, Scope.REQUEST),
+        (Scope.REQUEST, Scope.STEP, Scope.STEP),
+        (Scope.REQUEST, None, Scope.REQUEST),
+    ],
+)
 @pytest.mark.asyncio
 async def test_async_enter(provide_scope, start_scope, expected_scope):
     class MyProvider(Provider):
@@ -114,6 +157,94 @@ async def test_async_enter(provide_scope, start_scope, expected_scope):
         a = await container.get(ClassA)
         assert a
         assert a.dep == 100
+    assert a.closed
+
+
+@pytest.mark.parametrize(
+    ("provide_scope", "start_scope", "expected_scope"),
+    [
+        (Scope.SESSION, Scope.REQUEST, Scope.REQUEST),
+        (Scope.REQUEST, Scope.REQUEST, Scope.REQUEST),
+        (Scope.REQUEST, Scope.STEP, Scope.STEP),
+        (Scope.REQUEST, None, Scope.REQUEST),
+    ],
+)
+@pytest.mark.asyncio
+async def test_error_in_async_enter(
+    provide_scope,
+    start_scope,
+    expected_scope,
+):
+    exception = ValueError("Some exception")
+    mock = Mock()
+
+    class MyProvider(Provider):
+        a = provide(sync_gen_a, scope=provide_scope)
+
+        @provide(scope=provide_scope)
+        def get_int(self) -> Generator[None, int, None]:
+            caught_exc = yield 100
+            mock(caught_exc)
+
+    base_container = make_async_container(MyProvider())
+
+    try:
+        async with base_container(scope=start_scope) as container:
+            assert container.registry.scope is expected_scope
+            a = await container.get(ClassA)
+            assert a
+            assert a.dep == 100
+
+            raise exception
+    except ValueError as exc:
+        if exc != exception:
+            raise
+
+    mock.assert_called_with(exception)
+    assert a.closed
+
+
+@pytest.mark.parametrize(
+    ("provide_scope", "start_scope", "expected_scope"),
+    [
+        (Scope.SESSION, Scope.REQUEST, Scope.REQUEST),
+        (Scope.REQUEST, Scope.REQUEST, Scope.REQUEST),
+        (Scope.REQUEST, Scope.STEP, Scope.STEP),
+        (Scope.REQUEST, None, Scope.REQUEST),
+    ],
+)
+@pytest.mark.asyncio
+async def test_error_in_async_enter_with_async_gen(
+    provide_scope,
+    start_scope,
+    expected_scope,
+):
+    exception = ValueError("Some exception")
+    mock = Mock()
+
+    class MyProvider(Provider):
+        a = provide(async_gen_a, scope=provide_scope)
+
+        @provide(scope=provide_scope)
+        async def get_int(self) -> AsyncGenerator[int, None]:
+            caught_exc = yield 100
+            mock(caught_exc)
+
+    base_container = make_async_container(MyProvider())
+
+    try:
+        async with base_container(scope=start_scope) as container:
+            assert container.registry.scope is expected_scope
+            a = await container.get(ClassA)
+            assert a
+            assert a.dep == 100
+
+            raise exception
+    except ValueError as exc:
+        if exc != exception:
+            raise
+
+    mock.assert_called_with(exception)
     assert a.closed
 
 

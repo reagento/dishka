f011fcc3015a2106029afa5dc9c419a95711c838
IvanKirpichnikov <mmssvvvv570@gmail.com>
2024-08-04 15:37:41 +0300

    edit WithParents test

--- tests/unit/container/test_with_parents.py
+++ tests/unit/container/test_with_parents.py
@@ -1,15 +1,27 @@
 from abc import ABC
-from typing import Any, Generic, Protocol, TypeVar
+from typing import Annotated, Any, Generic, Protocol, TypeVar
 
 import pytest
 
-from dishka import Provider, Scope, make_container
+from dishka import (
+    DEFAULT_COMPONENT,
+    FromComponent,
+    Provider,
+    Scope,
+    make_container,
+)
 from dishka._adaptix.feature_requirement import HAS_PY_311
 from dishka.entities.with_parents import WithParents
 from dishka.exceptions import NoFactoryError
 
+if HAS_PY_311:
+    from typing import TypeVarTuple, Unpack
+
+    Ts = TypeVarTuple("TS")  # noqa: PLC0132
+
 T = TypeVar("T")
 B = TypeVar("B")
+
 
 def test_simple_inheritance() -> None:
     class A1: ...
@@ -27,20 +39,24 @@ def test_simple_inheritance() -> None:
 
 
 @pytest.mark.parametrize(
-    ("obj", "value"),
+    ("obj", "value", "component"),
     [
-        (type("A1", (Protocol,), {}), Protocol),
-        (type("A2", (object,), {}), object),
-        (type("A3", (ABC,), {}), ABC),
+        (type("A1", (Protocol,), {}), Protocol, DEFAULT_COMPONENT),
+        (type("A2", (object,), {}), object, DEFAULT_COMPONENT),
+        (type("A3", (ABC,), {}), ABC, DEFAULT_COMPONENT),
+        (type("A3", (ABC,), {}), ABC, "TTT"),
     ],
 )
-def test_ignore_parent_type(obj: Any, value: Any) -> None:
-    provider = Provider(scope=Scope.APP)
-    provider.provide(lambda: obj(), provides=WithParents[obj])
+def test_ignore_parent_type(obj: Any, value: Any, component: Any) -> None:
+    provider = Provider(scope=Scope.APP, component=component)
+    provider.provide(
+        source=lambda: obj(),
+        provides=Annotated[WithParents[obj], FromComponent(component)],
+    )
     container = make_container(provider)
 
     try:
-        container.get(value)
+        container.get(value, component=component)
     except NoFactoryError:
         pass
     else:
@@ -62,24 +78,26 @@ def test_type_var() -> None:
         is container.get(A1[str])
     )
 
+
+@pytest.mark.skipif(
+    not HAS_PY_311,
+    reason="test for python >= 3.11",
+)
+def test_type_var_tuple() -> None:
+    class A1(Generic[Unpack[Ts]]): ...
+    class A2(A1[str, int, type]): ...
+
+    provider = Provider(scope=Scope.APP)
+    provider.provide(lambda: A2(), provides=WithParents[A2])
+
+    container = make_container(provider)
+
+    assert (
+        container.get(A2)
+        is container.get(A1[str, int, type])
+    )
+
 if HAS_PY_311:
-    from typing import TypeVarTuple, Unpack
-    Ts = TypeVarTuple("TS")  # noqa: PLC0132
-
-    def test_type_var_tuple() -> None:
-        class A1(Generic[Unpack[Ts]]): ...
-        class A2(A1[str, int, type]): ...
-
-        provider = Provider(scope=Scope.APP)
-        provider.provide(lambda: A2(), provides=WithParents[A2])
-
-        container = make_container(provider)
-
-        assert (
-            container.get(A2)
-            is container.get(A1[str, int, type])
-        )
-
     class A1(Generic[T, Unpack[Ts]]): ...
     class A2(A1[str, int, type], int): ...
 
@@ -89,28 +107,36 @@ if HAS_PY_311:
     class C1(Generic[B, Unpack[Ts], T]): ...
     class C2(C1[int, type, str, tuple[str, ...]], int): ...
 
-    @pytest.mark.parametrize(
-        ("obj", "val1", "val2"),
-        [
-            (A2, A1[str, int, type], int),
-            (B2, B1[int, tuple[str, ...], type], int),
-            (C2, C1[int, type, str, tuple[str, ...]], int),
-        ],
-    )
-    def test_type_var_and_type_var_tuple(
-        obj: Any,
-        val1: Any,
-        val2: Any,
-    ) -> None:
-        provider = Provider(scope=Scope.APP)
-        provider.provide(lambda: obj(), provides=WithParents[obj])
-        container = make_container(provider)
+    params = [
+        (A2, A1[str, int, type], int),
+        (B2, B1[int, tuple[str, ...], type], int),
+        (C2, C1[int, type, str, tuple[str, ...]], int),
+    ]
+else:
+    params = []
 
-        assert (
-            container.get(obj)
-            is container.get(val1)
-            is container.get(val2)
-        )
+@pytest.mark.parametrize(
+    ("obj", "val1", "val2"),
+    params,
+)
+@pytest.mark.skipif(
+    not HAS_PY_311,
+reason="test for python >= 3.11",
+)
+def test_type_var_and_type_var_tuple(
+    obj: Any,
+    val1: Any,
+    val2: Any,
+) -> None:
+    provider = Provider(scope=Scope.APP)
+    provider.provide(lambda: obj(), provides=WithParents[obj])
+    container = make_container(provider)
+
+    assert (
+        container.get(obj)
+        is container.get(val1)
+        is container.get(val2)
+    )
 
 def test_deep_inheritance() -> None:
     class A1(Generic[T], float): ...

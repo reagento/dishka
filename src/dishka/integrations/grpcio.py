239d2247a4c25d83b3388defb95e6ba46321e4d3
Andrey Tikhonov <17@itishka.org>
2024-08-14 22:17:49 +0200

    grpc.aio support with context vars, cleanup tests, support generators in injection

--- src/dishka/integrations/grpcio.py
+++ src/dishka/integrations/grpcio.py
@@ -1,41 +1,36 @@
 __all__ = [
     "FromDishka",
     "inject",
-    "setup_dishka",
+    "DishkaAioInterceptor",
+    "DishkaAioInterceptor",
 ]
 
-from collections.abc import Callable, Iterator
-from typing import Any, ParamSpec, TypeVar, cast
+from collections.abc import Awaitable, Callable, Iterator
+from contextvars import ContextVar
+from inspect import isasyncgenfunction, iscoroutinefunction
+from typing import Any, ParamSpec, TypeVar
 
-import grpc._interceptor  # type: ignore[import-untyped]
+import grpc
 from google.protobuf import message
 
-from dishka import Container, FromDishka, Scope
+from dishka import AsyncContainer, Container, FromDishka, Scope
 from dishka.integrations.base import wrap_injection
 
 P = ParamSpec("P")
 RT = TypeVar("RT")
 
-
-def getter(args: tuple[Any, ...], _: dict[Any, Any]) -> Container:
-    iterator = (
-        arg._dishka_container  # noqa: SLF001
-        for arg in args
-        if isinstance(arg, grpc.ServicerContext)
-    )
-    # typing.cast because the iterator is not typed
-    return cast(Container, next(iterator))
+_dishka_scoped_container = ContextVar("_dishka_scoped_container")
 
 
 def inject(func: Callable[P, RT]) -> Callable[P, RT]:
     return wrap_injection(
         func=func,
-        is_async=False,
-        container_getter=getter,
+        is_async=iscoroutinefunction(func) or isasyncgenfunction(func),
+        container_getter=lambda _, __: _dishka_scoped_container.get(),
     )
 
 
-class ContainerInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
+class DishkaInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
     def __init__(self, container: Container) -> None:
         self._container = container
 
@@ -58,7 +53,7 @@ class ContainerInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
                 grpc.ServicerContext: context,
             }
             with self._container(context=context_) as container:
-                context._dishka_container = container  # noqa: SLF001
+                _dishka_scoped_container.set(container)
                 return rpc_handler.unary_unary(request, context)
 
         def stream_unary_behavior(
@@ -70,8 +65,10 @@ class ContainerInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
                 context=context_,
                 scope=Scope.SESSION,
             ) as container:
-                context._dishka_container = container  # noqa: SLF001
-                return rpc_handler.stream_unary(request_iterator, context)
+                _dishka_scoped_container.set(container)
+                return rpc_handler.stream_unary(
+                    request_iterator, context,
+                )
 
         def unary_stream_behavior(
             request: message.Message,
@@ -81,12 +78,9 @@ class ContainerInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
                 message.Message: request,
                 grpc.ServicerContext: context,
             }
-            with self._container(
-                context=context_,
-                scope=Scope.SESSION,
-            ) as container:
-                context._dishka_container = container  # noqa: SLF001
-                return rpc_handler.unary_stream(request, context)
+            with self._container(context=context_) as container:
+                _dishka_scoped_container.set(container)
+                yield from rpc_handler.unary_stream(request, context)
 
         def stream_stream_behavior(
             request_iterator: Iterator[message.Message],
@@ -97,8 +91,8 @@ class ContainerInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
                 context=context_,
                 scope=Scope.SESSION,
             ) as container:
-                context._dishka_container = container  # noqa: SLF001
-                return rpc_handler.stream_stream(request_iterator, context)
+                _dishka_scoped_container.set(container)
+                yield from rpc_handler.stream_stream(request_iterator, context)
 
         if rpc_handler.unary_unary:
             return grpc.unary_unary_rpc_method_handler(
@@ -128,20 +122,100 @@ class ContainerInterceptor(grpc.ServerInterceptor):  # type: ignore[misc]
         return rpc_handler
 
 
-def setup_dishka(container: Container, server: grpc.Server) -> None:
-    interceptor = ContainerInterceptor(container)
+class DishkaAioInterceptor(grpc.aio.ServerInterceptor):  # type: ignore[misc]
+    def __init__(self, container: AsyncContainer) -> None:
+        self._container = container
 
-    interceptor_pipeline = server._state.interceptor_pipeline  # noqa: SLF001
+    async def intercept_service(  # noqa: C901
+        self,
+        continuation: Callable[
+            [grpc.HandlerCallDetails],
+            Awaitable[grpc.RpcMethodHandler],
+        ],
+        handler_call_details: grpc.HandlerCallDetails,
+    ) -> grpc.RpcMethodHandler:
+        rpc_handler = await continuation(handler_call_details)
 
-    if interceptor_pipeline is None:
-        interceptor_pipeline = grpc._interceptor._ServicePipeline(  # noqa: SLF001
-            [interceptor],
-        )
+        async def unary_unary_behavior(
+            request: message.Message,
+            context: grpc.ServicerContext,
+        ) -> Any:
+            context_ = {
+                message.Message: request,
+                grpc.ServicerContext: context,
+            }
+            async with self._container(context=context_) as container:
+                _dishka_scoped_container.set(container)
+                return await rpc_handler.unary_unary(request, context)
 
-    else:
-        interceptors = [interceptor, *interceptor_pipeline.interceptors]
-        interceptor_pipeline = grpc._interceptor._ServicePipeline(  # noqa: SLF001
-            interceptors,
-        )
+        async def stream_unary_behavior(
+            request_iterator: Iterator[message.Message],
+            context: grpc.ServicerContext,
+        ) -> Any:
+            context_ = {grpc.ServicerContext: context}
+            async with self._container(
+                context=context_,
+                scope=Scope.SESSION,
+            ) as container:
+                _dishka_scoped_container.set(container)
+                return await rpc_handler.stream_unary(
+                    request_iterator, context,
+                )
 
-    server._state.interceptor_pipeline = interceptor_pipeline  # noqa: SLF001
+        async def unary_stream_behavior(
+            request: message.Message,
+            context: grpc.ServicerContext,
+        ) -> Any:
+            context_ = {
+                message.Message: request,
+                grpc.ServicerContext: context,
+            }
+            async with self._container(
+                context=context_,
+                scope=Scope.REQUEST,
+            ) as container:
+                _dishka_scoped_container.set(container)
+                stream = rpc_handler.unary_stream(request, context)
+                async for result in stream:
+                    yield result
+
+        async def stream_stream_behavior(
+            request_iterator: Iterator[message.Message],
+            context: grpc.ServicerContext,
+        ) -> Any:
+            context_ = {grpc.ServicerContext: context}
+            async with self._container(
+                context=context_,
+                scope=Scope.SESSION,
+            ) as container:
+                _dishka_scoped_container.set(container)
+                stream = rpc_handler.stream_stream(request_iterator, context)
+                async for result in stream:
+                    yield result
+
+        if rpc_handler.unary_unary:
+            return grpc.unary_unary_rpc_method_handler(
+                unary_unary_behavior,
+                rpc_handler.request_deserializer,
+                rpc_handler.response_serializer,
+            )
+        elif rpc_handler.stream_unary:
+            return grpc.stream_unary_rpc_method_handler(
+                stream_unary_behavior,
+                rpc_handler.request_deserializer,
+                rpc_handler.response_serializer,
+            )
+        elif rpc_handler.unary_stream:
+            return grpc.unary_stream_rpc_method_handler(
+                unary_stream_behavior,
+                rpc_handler.request_deserializer,
+                rpc_handler.response_serializer,
+            )
+        elif rpc_handler.stream_stream:
+            return grpc.stream_stream_rpc_method_handler(
+                stream_stream_behavior,
+                rpc_handler.request_deserializer,
+                rpc_handler.response_serializer,
+            )
+
+        return rpc_handler

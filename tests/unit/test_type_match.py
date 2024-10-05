a61170907051c717a0e37ec63e9d3de869ab8f20
Andrey Tikhonov <17@itishka.org>
2024-10-05 23:14:24 +0200

    check multiple typevars together

--- tests/unit/test_type_match.py
+++ tests/unit/test_type_match.py
@@ -5,6 +5,8 @@ import pytest
 from dishka.dependency_source.decorator import is_broader_or_same_type
 
 T = TypeVar("T")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
 
 
 class AGeneric(Generic[T]): ...
@@ -14,6 +16,9 @@ class SubAGeneric(AGeneric[T], Generic[T]): ...
 
 
 class BGeneric(Generic[T]): ...
+
+
+class Mutiple(Generic[T, T2, T3]): ...
 
 
 class C: ...
@@ -42,6 +47,21 @@ TSubCCD = TypeVar("TSubCCD", "C", SubC, D)
         (AGeneric[TC], AGeneric[C], True),
         (AGeneric[TC], AGeneric[SubC], True),
         (AGeneric[C], BGeneric[C], False),
+        (
+            Mutiple[AGeneric[T], AGeneric[AGeneric[T2]], T3],
+            Mutiple[AGeneric[int], AGeneric[AGeneric[T]], T2],
+            True,
+        ),
+        (
+            Mutiple[AGeneric[T], AGeneric[AGeneric[T]], T3],
+            Mutiple[AGeneric[int], AGeneric[AGeneric[int]], T2],
+            True,
+        ),
+        (
+            Mutiple[AGeneric[T], AGeneric[AGeneric[T]], T3],
+            Mutiple[AGeneric[int], AGeneric[AGeneric[str]], T2],
+            False,
+        ),
     ],
 )
 def test_is_broader_or_same_type(*, first: T, second: T, match: bool):

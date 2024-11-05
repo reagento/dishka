d82138416f7168033efdedd0daf9e0c2b7e71410
Andrey Tikhonov <17@itishka.org>
2024-11-05 22:35:29 +0100

    add annotations for provides multiple

--- src/dishka/entities/provides_marker.py
+++ src/dishka/entities/provides_marker.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import sys
 import threading
-from typing import TYPE_CHECKING, Generic, TypeVar
+from typing import TYPE_CHECKING, Any, Generic, TypeVar
 
 __all__ = ["AnyOf", "ProvideMultiple"]
 
@@ -14,14 +14,16 @@ if sys.version_info >= (3, 11):
     class ProvideMultiple(Generic[Unpack[Variants]]):
         pass
 else:
+    # in this case we simulate generics with variadic typevars
+    # by suppressing builtin checks of parameters length
     Variants = TypeVar("Variants")
     provides_lock = threading.Lock()
 
     class ProvideMultiple(Generic[Variants]):
-        def __class_getitem__(cls, item):
+        def __class_getitem__(cls, item: tuple[Any]):
             with provides_lock:
-                cls.__parameters__ = [Variants]*len(item)
-                return super().__class_getitem__(item)
+                cls.__parameters__ = [Variants]*len(item)  # type: ignore[attr-defined, misc]
+                return super().__class_getitem__(item)  # type: ignore[misc]
 
 
 if TYPE_CHECKING:

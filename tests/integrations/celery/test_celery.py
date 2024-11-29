8f0399d5d6c1299f7a72fec5b06c40964ccdcec6
Apostol Fet <maksim170901@gmail.com>
2024-11-29 21:53:44 +0300

    Test celery integration with delay, bind task, args/kwargs

--- tests/integrations/celery/test_celery.py
+++ tests/integrations/celery/test_celery.py
@@ -2,7 +2,7 @@ from collections.abc import Iterator
 from contextlib import contextmanager
 from unittest.mock import Mock
 
-from celery import Celery
+from celery import Celery, Task
 
 from dishka import FromDishka, Provider, make_container
 from dishka.integrations.celery import DishkaTask, inject, setup_dishka
@@ -12,6 +12,7 @@ from ..common import APP_DEP_VALUE, AppDep, AppProvider
 @contextmanager
 def dishka_app(provider: Provider) -> Iterator[Celery]:
     app = Celery()
+    app.conf["task_always_eager"] = True
     container = make_container(provider)
     setup_dishka(container=container, app=app)
     yield app
@@ -21,6 +22,7 @@ def dishka_app(provider: Provider) -> Iterator[Celery]:
 @contextmanager
 def dishka_auto_app(provider: Provider) -> Iterator[Celery]:
     app = Celery(task_cls=DishkaTask)
+    app.conf["task_always_eager"] = True
     container = make_container(provider)
     setup_dishka(container=container, app=app)
     yield app
@@ -35,10 +37,32 @@ def handle_with_app(
     return a
 
 
+def handle_with_bind(
+    task: Task,
+    mock: FromDishka[Mock],
+):
+    mock(task)
+
+
+def handle_with_args(*args, mock: FromDishka[Mock]):
+    mock(args)
+
+
+def handle_with_kwargs(
+    mock: FromDishka[Mock],
+    **kwargs,
+):
+    mock(kwargs)
+
+
+def handle_with_keyword_only(*, x: int, mock: FromDishka[Mock]):
+    mock(x)
+
+
 def test_app_dependency(app_provider: AppProvider):
-    with dishka_auto_app(app_provider) as app:
+    with dishka_app(app_provider) as app:
         task = app.task(inject(handle_with_app))
-        result = task.apply()
+        result = task.delay()
 
         assert result.get() == APP_DEP_VALUE
         app_provider.mock.assert_called_with(APP_DEP_VALUE)
@@ -50,9 +74,95 @@ def test_app_auto_dependency(app_provider: AppProvider):
     with dishka_auto_app(app_provider) as app:
         task = app.task(handle_with_app)
 
-        result = task.apply()
+        result = task.delay()
 
         assert result.get() == APP_DEP_VALUE
         app_provider.mock.assert_called_with(APP_DEP_VALUE)
         app_provider.app_released.assert_not_called()
     app_provider.app_released.assert_called()
+
+
+def test_with_bind(app_provider: AppProvider):
+    with dishka_app(app_provider) as app:
+        task = app.task(bind=True)(inject(handle_with_bind))
+        result = task.delay()
+
+        result.get()
+        app_provider.mock.assert_called_with(task)
+
+
+def test_with_bind_auto(app_provider: AppProvider):
+    with dishka_auto_app(app_provider) as app:
+        task = app.task(bind=True)(handle_with_bind)
+
+        result = task.delay()
+
+        result.get()
+        app_provider.mock.assert_called_with(task)
+
+
+def test_with_args(app_provider: AppProvider):
+    args = (5, 6)
+
+    with dishka_app(app_provider) as app:
+        task = app.task(inject(handle_with_args))
+        result = task.delay(*args)
+
+        result.get()
+        app_provider.mock.assert_called_with(args)
+
+
+def test_with_args_auto(app_provider: AppProvider):
+    args = (5, 6)
+
+    with dishka_auto_app(app_provider) as app:
+        task = app.task(handle_with_args)
+
+        result = task.delay(*args)
+
+        result.get()
+        app_provider.mock.assert_called_with(args)
+
+
+def test_with_kwargs(app_provider: AppProvider):
+    kwargs = {"x": 5, "y": 6}
+
+    with dishka_app(app_provider) as app:
+        task = app.task(inject(handle_with_kwargs))
+        result = task.delay(**kwargs)
+
+        result.get()
+        app_provider.mock.assert_called_with(kwargs)
+
+
+def test_with_kwargs_auto(app_provider: AppProvider):
+    kwargs = {"x": 5, "y": 6}
+
+    with dishka_auto_app(app_provider) as app:
+        task = app.task(handle_with_kwargs)
+
+        result = task.delay(**kwargs)
+
+        result.get()
+        app_provider.mock.assert_called_with(kwargs)
+
+
+def test_with_keyword_only(app_provider: AppProvider):
+    keyword = {"x": 6}
+    with dishka_app(app_provider) as app:
+        task = app.task(inject(handle_with_keyword_only))
+        result = task.delay(**keyword)
+
+        result.get()
+        app_provider.mock.assert_called_with(keyword.get("x"))
+
+
+def test_with_keyword_only_only_auto(app_provider: AppProvider):
+    keyword = {"x": 6}
+    with dishka_auto_app(app_provider) as app:
+        task = app.task(handle_with_keyword_only)
+
+        result = task.delay(**keyword)
+
+        result.get()
+        app_provider.mock.assert_called_with(keyword.get("x"))

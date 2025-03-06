6df85f058ae33fdca926795a7ebbf2f784c26761
Nikita Pastukhov <diementros@yandex.ru>
2025-03-06 18:26:15 +0300

    chore: add aliases for nox session groups

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,107 @@
 import sys
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from dataclasses import dataclass
+from itertools import chain
+from typing import Protocol
 
 import nox
 
 nox.options.default_venv_backend = "uv"
 nox.options.reuse_existing_virtualenvs = True
 
-CMD = ("pytest", "--cov=dishka", "--cov-append", "--cov-report=term-missing", "-v")
+CMD = (
+    "pytest",
+    "--cov=dishka",
+    "--cov-append",
+    "--cov-report=term-missing",
+    "-v",
+)
 INSTALL_CMD = ("pytest", "pytest-cov", "-e", ".")
 
 
 @dataclass(frozen=True, slots=True)
-class IntegrationEnv:
+class Constraint:
+    reason: str = ""
+    condition: Callable[[], bool] = lambda: True
+
+
+@dataclass(frozen=True, slots=True)
+class Requirement:
+    req: str
+    tests: str
+
+
+class NoxEnvironment(Protocol):
+    constraint: Constraint | None
+
+    @property
+    def requirements(self) -> Iterable[Requirement]: ...
+
+    @property
+    def name(self) -> str: ...
+
+    @property
+    def tags(self) -> list[str]: ...
+
+
+@dataclass(frozen=True, slots=True)
+class IntegrationEnv(NoxEnvironment):
     library: str
     version: str
-    constraint: Callable[[], bool] = lambda: True
+    constraint: Constraint | None = None
 
-    def get_req(self) -> str:
-        return f"requirements/{self.library.replace('_', '-')}-{self.version}.txt"
+    @property
+    def requirements(self) -> Iterable[Requirement]:
+        return (
+            Requirement(
+                req=(
+                    "requirements/"
+                    f"{self.library.replace('_', '-')}"
+                    f"-{self.version}.txt"
+                ),
+                tests=f"tests/integrations/{self.library}",
+            ),
+        )
 
-    def get_tests(self) -> str:
-        return f"tests/integrations/{self.library}"
+    @property
+    def name(self) -> str:
+        return f"{self.library}_{self.version}"
 
+    @property
+    def tags(self) -> list[str]:
+        return [self.library, "latest" if self.version == "latest" else "ci"]
+
+
+@dataclass(frozen=True, slots=True)
+class IntegrationAlias(NoxEnvironment):
+    library: str
+    integrations: list[IntegrationEnv]
+    constraint: Constraint | None = None
+
+    @property
+    def requirements(self) -> Iterable[Requirement]:
+        return chain(
+            req
+            for integration in self.integrations
+            for req in integration.requirements
+        )
+
+    @property
+    def name(self) -> str:
+        return self.library
+
+    @property
+    def tags(self) -> list[str]:
+        return [self.library]
+
+
+constraint_3_13 = Constraint(
+    "Skip tests on python 3.13 due to compatibility issues",
+    lambda: sys.version_info < (3, 13),
+)
 
 INTEGRATIONS = [
-    IntegrationEnv("aiogram", "330", lambda: sys.version_info < (3, 13)),
+    IntegrationEnv("aiogram", "330", constraint_3_13),
     IntegrationEnv("aiogram", "3140"),
     IntegrationEnv("aiogram", "latest"),
     IntegrationEnv("aiogram_dialog", "210"),
@@ -39,13 +115,18 @@ INTEGRATIONS = [
     IntegrationEnv("fastapi", "0096"),
     IntegrationEnv("fastapi", "0109"),
     IntegrationEnv("fastapi", "latest"),
-    IntegrationEnv("faststream", "047", lambda: sys.version_info < (3, 13)),
-    IntegrationEnv("faststream", "050", lambda: sys.version_info < (3, 13)),
-    IntegrationEnv("faststream", "0529"),
-    IntegrationEnv("faststream", "latest"),
+    IntegrationAlias(
+        "faststream",
+        integrations=[
+            IntegrationEnv("faststream", "047", constraint_3_13),
+            IntegrationEnv("faststream", "050", constraint_3_13),
+            IntegrationEnv("faststream", "0529"),
+            IntegrationEnv("faststream", "latest"),
+        ],
+    ),
     IntegrationEnv("flask", "302"),
     IntegrationEnv("flask", "latest"),
-    IntegrationEnv("grpcio", "1641", lambda: sys.version_info < (3, 13)),
+    IntegrationEnv("grpcio", "1641", constraint_3_13),
     IntegrationEnv("grpcio", "1680"),
     IntegrationEnv("grpcio", "latest"),
     IntegrationEnv("litestar", "230"),
@@ -67,28 +148,40 @@ INTEGRATIONS = [
 def integrations_base(session: nox.Session) -> None:
     session.install(
         *INSTALL_CMD,
-        "-r", "requirements/test.txt",
+        "-r",
+        "requirements/test.txt",
     )
     session.run(*CMD, "tests/integrations/base")
 
 
+def register_env(env: NoxEnvironment) -> None:
+    @nox.session(name=env.name, tags=env.tags)
+    def session(session: nox.Session, env: NoxEnvironment = env) -> None:
+        if env.constraint and not env.constraint.condition():
+            session.skip(env.constraint.reason)
+
+        for req in env.requirements:
+            session.install(*INSTALL_CMD, "-r", req.req)
+            session.run(*CMD, req.tests)
+
+
 for env in INTEGRATIONS:
-    @nox.session(
-        name=f"{env.library}_{env.version}",
-        tags=[env.library, "latest" if env.version == "latest" else "ci"],
-    )
-    def session(session: nox.Session, env=env) -> None:
-        if not env.constraint():
-            session.skip("Skip tests on python 3.13 due to compatibility issues")
-        session.install(*INSTALL_CMD, "-r", env.get_req())
-        session.run(*CMD, env.get_tests())
+    if isinstance(env, IntegrationAlias):
+        register_env(env)
+
+        for e in env.integrations:
+            register_env(e)
+
+    else:
+        register_env(env)
 
 
 @nox.session(tags=["ci"])
 def unit(session: nox.Session) -> None:
     session.install(
         *INSTALL_CMD,
-        "-r", "requirements/test.txt",
+        "-r",
+        "requirements/test.txt",
     )
     session.run(*CMD, "tests/unit")
 
@@ -97,6 +190,7 @@ def unit(session: nox.Session) -> None:
 def real_world(session: nox.Session) -> None:
     session.install(
         *INSTALL_CMD,
-        "-r", "examples/real_world/requirements_test.txt",
+        "-r",
+        "examples/real_world/requirements_test.txt",
     )
     session.run(*CMD, "examples/real_world/tests/")

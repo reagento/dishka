91d56f5330078b995a296ccf97e5a2d8e673de3c
Nikita Pastukhov <diementros@yandex.ru>
2025-03-06 19:29:19 +0300

    chore: revert noxfile changes

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,6 @@
 import sys
-from collections.abc import Callable, Iterable
+from collections.abc import Callable
 from dataclasses import dataclass
-from itertools import chain
-from typing import Protocol
 
 import nox
 
@@ -26,73 +24,18 @@ class Constraint:
 
 
 @dataclass(frozen=True, slots=True)
-class Requirement:
-    req: str
-    tests: str
-
-
-class NoxEnvironment(Protocol):
-    constraint: Constraint | None
-
-    @property
-    def requirements(self) -> Iterable[Requirement]: ...
-
-    @property
-    def name(self) -> str: ...
-
-    @property
-    def tags(self) -> list[str]: ...
-
-
-@dataclass(frozen=True, slots=True)
-class IntegrationEnv(NoxEnvironment):
+class IntegrationEnv:
     library: str
     version: str
     constraint: Constraint | None = None
 
-    @property
-    def requirements(self) -> Iterable[Requirement]:
+    def get_req(self) -> str:
         return (
-            Requirement(
-                req=(
-                    "requirements/"
-                    f"{self.library.replace('_', '-')}"
-                    f"-{self.version}.txt"
-                ),
-                tests=f"tests/integrations/{self.library}",
-            ),
+            f"requirements/{self.library.replace('_', '-')}-{self.version}.txt"
         )
 
-    @property
-    def name(self) -> str:
-        return f"{self.library}_{self.version}"
-
-    @property
-    def tags(self) -> list[str]:
-        return [self.library, "latest" if self.version == "latest" else "ci"]
-
-
-@dataclass(frozen=True, slots=True)
-class IntegrationAlias(NoxEnvironment):
-    library: str
-    integrations: list[IntegrationEnv]
-    constraint: Constraint | None = None
-
-    @property
-    def requirements(self) -> Iterable[Requirement]:
-        return chain(
-            req
-            for integration in self.integrations
-            for req in integration.requirements
-        )
-
-    @property
-    def name(self) -> str:
-        return self.library
-
-    @property
-    def tags(self) -> list[str]:
-        return [self.library]
+    def get_tests(self) -> str:
+        return f"tests/integrations/{self.library}"
 
 
 constraint_3_13 = Constraint(
@@ -115,15 +58,10 @@ INTEGRATIONS = [
     IntegrationEnv("fastapi", "0096"),
     IntegrationEnv("fastapi", "0109"),
     IntegrationEnv("fastapi", "latest"),
-    IntegrationAlias(
-        "faststream",
-        integrations=[
-            IntegrationEnv("faststream", "047", constraint_3_13),
-            IntegrationEnv("faststream", "050", constraint_3_13),
-            IntegrationEnv("faststream", "0529"),
-            IntegrationEnv("faststream", "latest"),
-        ],
-    ),
+    IntegrationEnv("faststream", "047", constraint_3_13),
+    IntegrationEnv("faststream", "050", constraint_3_13),
+    IntegrationEnv("faststream", "0529"),
+    IntegrationEnv("faststream", "latest"),
     IntegrationEnv("flask", "302"),
     IntegrationEnv("flask", "latest"),
     IntegrationEnv("grpcio", "1641", constraint_3_13),
@@ -154,34 +92,24 @@ def integrations_base(session: nox.Session) -> None:
     session.run(*CMD, "tests/integrations/base")
 
 
-def register_env(env: NoxEnvironment) -> None:
-    @nox.session(name=env.name, tags=env.tags)
-    def session(session: nox.Session, env: NoxEnvironment = env) -> None:
+for env in INTEGRATIONS:
+    @nox.session(
+        name=f"{env.library}_{env.version}",
+        tags=[env.library, "latest" if env.version == "latest" else "ci"],
+    )
+    def session(session: nox.Session, env=env) -> None:
         if env.constraint and not env.constraint.condition():
             session.skip(env.constraint.reason)
 
-        for req in env.requirements:
-            session.install(*INSTALL_CMD, "-r", req.req)
-            session.run(*CMD, req.tests)
-
-
-for env in INTEGRATIONS:
-    if isinstance(env, IntegrationAlias):
-        register_env(env)
-
-        for e in env.integrations:
-            register_env(e)
-
-    else:
-        register_env(env)
+        session.install(*INSTALL_CMD, "-r", env.get_req())
+        session.run(*CMD, env.get_tests())
 
 
 @nox.session(tags=["ci"])
 def unit(session: nox.Session) -> None:
     session.install(
         *INSTALL_CMD,
-        "-r",
-        "requirements/test.txt",
+        "-r", "requirements/test.txt",
     )
     session.run(*CMD, "tests/unit")
 
@@ -190,7 +118,6 @@ def unit(session: nox.Session) -> None:
 def real_world(session: nox.Session) -> None:
     session.install(
         *INSTALL_CMD,
-        "-r",
-        "examples/real_world/requirements_test.txt",
+        "-r", "examples/real_world/requirements_test.txt",
     )
     session.run(*CMD, "examples/real_world/tests/")

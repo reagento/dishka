75f6c04d993c1a15df8ccf3f7f45aaf4118a4656
ApostolFet <maksim170901@gmail.com>
2025-03-08 00:14:50 +0300

    fix(celery): use manage_scope instead of signals for support worker threads

--- src/dishka/integrations/celery.py
+++ src/dishka/integrations/celery.py
@@ -6,17 +6,15 @@ __all__ = [
 ]
 
 from collections.abc import Callable
-from typing import Any, Final, ParamSpec, TypeVar
+from typing import Final, ParamSpec, TypeVar
 
 from celery import Celery, Task, current_app
-from celery.signals import task_postrun, task_prerun
 from celery.utils.functional import head_from_fun
 
 from dishka import Container, FromDishka
 from dishka.integrations.base import is_dishka_injected, wrap_injection
 
 CONTAINER_NAME: Final = "dishka_container"
-CONTAINER_NAME_REQ: Final = "dishka_container_req"
 
 
 T = TypeVar("T")
@@ -28,40 +26,14 @@ def inject(func: Callable[P, T]) -> Callable[P, T]:
         func=func,
         is_async=False,
         container_getter=lambda args, kwargs: current_app.conf[
-            CONTAINER_NAME_REQ
+            CONTAINER_NAME
         ],
+        manage_scope=True,
     )
 
 
 def setup_dishka(container: Container, app: Celery):
     app.conf[CONTAINER_NAME] = container
-
-
-@task_prerun.connect()
-def enter_scope(
-    task_id,
-    task: Task,
-    args,
-    kwargs: dict[str, Any],
-    **other,
-) -> None:
-    if CONTAINER_NAME in task.app.conf:
-        container: Container = task.app.conf[CONTAINER_NAME]
-        task.app.conf[CONTAINER_NAME_REQ] = container().__enter__()
-
-
-@task_postrun.connect()
-def exit_scope(
-    task_id,
-    task,
-    args,
-    kwargs: dict[str, Any],
-    retval,
-    **other,
-) -> None:
-    if CONTAINER_NAME_REQ in task.app.conf:
-        container: Container = task.app.conf.pop(CONTAINER_NAME_REQ)
-        container.close()
 
 
 class DishkaTask(Task):

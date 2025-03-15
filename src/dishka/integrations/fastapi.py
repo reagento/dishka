ae3c6fe235441de2f7c2380ee9739e00ae2a89f1
Andrey Tikhonov <17@itishka.org>
2025-03-15 23:20:34 +0100

    Support sync views with async container in FastAPI

--- src/dishka/integrations/fastapi.py
+++ src/dishka/integrations/fastapi.py
@@ -7,21 +7,65 @@ __all__ = [
 ]
 
 from collections.abc import Callable
-from inspect import Parameter
-from typing import Any, ParamSpec, TypeVar, get_type_hints
+from inspect import (
+    Parameter,
+    isasyncgenfunction,
+    iscoroutinefunction,
+    signature,
+)
+from typing import Annotated, Any, ParamSpec, TypeVar, get_type_hints
 
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import Depends, FastAPI, Request, WebSocket
 from fastapi.routing import APIRoute
 
-from dishka import AsyncContainer, FromDishka, Provider, Scope, from_context
-from .base import wrap_injection
+from dishka import (
+    AsyncContainer,
+    DependencyKey,
+    FromDishka,
+    Provider,
+    Scope,
+    from_context,
+)
+from .base import default_parse_dependency, wrap_injection
 from .starlette import ContainerMiddleware
 
 T = TypeVar("T")
 P = ParamSpec("P")
 
+def _async_depends(dependency: DependencyKey) -> Any:
+    async def dishka_depends(request: Request) -> dependency:
+        return await request.state.dishka_container.get(
+            dependency.type_hint, component=dependency.component,
+        )
+    return Annotated[dependency.type_hint, Depends(dishka_depends)]
+
+
+def _replace_depends(
+    func: Callable[P, T],
+        depends_factory: Callable[[DependencyKey],Any],
+) -> Callable[P, T]:
+    hints = get_type_hints(func, include_extras=True)
+    func_signature = signature(func)
+
+    new_params = []
+    for name, param in func_signature.parameters.items():
+        hint = hints.get(name, Any)
+        dep = default_parse_dependency(param, hint)
+        if dep is None:
+            new_params.append(param)
+            continue
+        new_dep = depends_factory(dep)
+        hints[name] = new_dep
+        new_params.append(param.replace(annotation=new_dep))
+    func.__signature__ = func_signature.replace(parameters=new_params)
+    func.__annotations__ = hints
+    return func
+
 
 def inject(func: Callable[P, T]) -> Callable[P, T]:
+    if not iscoroutinefunction(func) and not isasyncgenfunction(func):
+        return _replace_depends(func, _async_depends)
+
     hints = get_type_hints(func)
     request_hint = next(
         (name for name, hint in hints.items() if hint is Request),

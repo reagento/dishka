202b00d5f68167eee4fbbd9325fb57c6d8ee35df
Andrey Tikhonov <17@itishka.org>
2025-09-20 20:06:52 +0200

    Activation check logic

--- src/dishka/entities/activator.py
+++ src/dishka/entities/activator.py
@@ -1,15 +1,43 @@
+from abc import abstractmethod
 from collections.abc import Callable
-from typing import Any, NamedTuple, TypeAlias
+from typing import Any, NamedTuple, Protocol, TypeAlias
 
 from dishka.entities.component import Component
 from dishka.entities.key import DependencyKey
 
 
+class ActivationBuilder(Protocol):
+    @abstractmethod
+    def has_active(
+        self,
+        key: DependencyKey,
+        request_stack: list[DependencyKey],
+    ) -> bool:
+        raise NotImplementedError
+
+
 class ActivationCtx(NamedTuple):
     container_ctx: dict[Any, Any] | None
     container_key: DependencyKey
+    key: DependencyKey
     registered_deps: list[DependencyKey]
-    component: Component
+    builder: ActivationBuilder
+    request_stack: list[DependencyKey]
 
 
 ActivationFunc: TypeAlias = Callable[[ActivationCtx], bool]
+
+
+class Has:
+    def __init__(
+        self,
+        cls: Any, *,
+        component: Component | None = None,
+    ) -> None:
+        self.key = DependencyKey(cls, component=component)
+
+    def __call__(self, ctx: ActivationCtx) -> bool:
+        key = self.key.with_component(ctx.key.component)
+        if key in ctx.request_stack:  # cycle
+            return True
+        return ctx.builder.has_active(key, ctx.request_stack)
